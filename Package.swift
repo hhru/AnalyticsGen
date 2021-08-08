7ef62187b06ce901fc9030aa5f75935a968a2a7a
Timur Shafigullin <hometim55@gmail.com>
2021-08-08 10:03:33 +0300

    Mint support (#11)
    * Add Mint support
    
    * Add missing comma

--- Package.swift
+++ Package.swift
@@ -8,6 +8,10 @@ let package = Package(
     platforms: [
        .macOS(.v10_12)
     ],
+    products: [
+        .executable(name: "analyticsgen", targets: ["AnalyticsGen"]),
+        .library(name: "AnalyticsGenTools", targets: ["AnalyticsGenTools"])
+    ],
     dependencies: [
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "6.0.0"),
         .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
