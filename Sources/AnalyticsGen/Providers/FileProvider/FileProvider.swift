5fd68e430d4441f1e8ff2e8c5ec97dd9f42dac83
Iaroslav Pasternak <i.pasternak@hh.ru>
2022-09-09 13:38:25 +0300

    Add new methods to FileProvider

--- Sources/AnalyticsGen/Providers/FileProvider/FileProvider.swift
+++ Sources/AnalyticsGen/Providers/FileProvider/FileProvider.swift
@@ -6,9 +6,11 @@ protocol FileProvider {
 
     func readFile<FileContent: Decodable>(at path: String) throws -> FileContent
     func readFile<FileContent: Decodable>(at path: String) -> Promise<FileContent>
+    func readFile<FileContent: Decodable>(at path: String, type: FileContent.Type) -> Promise<FileContent>
 
     func readFileIfExists<FileContent: Decodable>(at path: String) throws -> FileContent?
     func readFileIfExists<FileContent: Decodable>(at path: String) -> Promise<FileContent?>
+    func readFileIfExists<FileContent: Decodable>(at path: String, type: FileContent.Type) -> Promise<FileContent?>
 
     func writeFile<FileContent: Encodable>(content: FileContent, at path: String) throws
 }
@@ -24,10 +26,18 @@ extension FileProvider {
             seal.fulfill(try readFile(at: path))
         }
     }
+    
+    func readFile<FileContent: Decodable>(at path: String, type: FileContent.Type) -> Promise<FileContent> {
+        readFile(at: path)
+    }
 
     func readFileIfExists<FileContent: Decodable>(at path: String) -> Promise<FileContent?> {
         Promise { seal in
             seal.fulfill(try readFileIfExists(at: path))
         }
+    }
+    
+    func readFileIfExists<FileContent: Decodable>(at path: String, type: FileContent.Type) -> Promise<FileContent?> {
+        readFileIfExists(at: path)
     }
 }
