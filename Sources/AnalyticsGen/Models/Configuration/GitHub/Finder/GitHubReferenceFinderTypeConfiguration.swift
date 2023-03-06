66c3bb07adb9e80527c9566517f0c1a9bf95cc6c
Timur Shafigullin <hometim55@gmail.com>
2023-03-06 15:32:57 +0300

    Optional branch in last_merged finder

--- Sources/AnalyticsGen/Models/Configuration/GitHub/Finder/GitHubReferenceFinderTypeConfiguration.swift
+++ Sources/AnalyticsGen/Models/Configuration/GitHub/Finder/GitHubReferenceFinderTypeConfiguration.swift
@@ -2,7 +2,7 @@ import Foundation
 
 enum GitHubReferenceFinderTypeConfiguration: Equatable {
     case matchedTag(source: FinderSourceConfiguration, branchRegex: String)
-    case lastMerged(branch: String, mergeCommitCount: Int, branchRegex: String)
+    case lastMerged(branch: String?, mergeCommitCount: Int, branchRegex: String)
     case lastTag
     case lastCommit(branch: String)
 }
