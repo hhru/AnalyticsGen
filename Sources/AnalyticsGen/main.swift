ace8a8b4db208ae6426dd49131ac0e6348b6da69
Timur Shafigullin <hometim55@gmail.com>
2023-03-09 17:58:59 +0300

    Version bump

--- Sources/AnalyticsGen/main.swift
+++ Sources/AnalyticsGen/main.swift
@@ -20,6 +20,6 @@ private extension String {
 
     // MARK: - Type Properties
 
-    static let version = "0.5.6"
+    static let version = "0.5.7"
     static let description = "Generate analytics code for you Swift iOS project"
 }
