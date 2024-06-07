d1d61945ffb7a0b82b691d8c92d3449578724019
Timur Shafigullin <hometim55@gmail.com>
2024-06-07 13:58:15 +0300

    Release 0.5.10

--- Sources/AnalyticsGen/main.swift
+++ Sources/AnalyticsGen/main.swift
@@ -20,6 +20,6 @@ private extension String {
 
     // MARK: - Type Properties
 
-    static let version = "0.5.9"
+    static let version = "0.5.10"
     static let description = "Generate analytics code for you Swift iOS project"
 }
