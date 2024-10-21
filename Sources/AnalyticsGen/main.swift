cf96367b1f9e1d7fe9821b6212a6cb489eccc09d
Виталий Барабанов <hurmavbatone+hhGit@gmail.com>
2024-10-21 16:24:33 +0300

    Release 0.5.13 (#64)

--- Sources/AnalyticsGen/main.swift
+++ Sources/AnalyticsGen/main.swift
@@ -20,6 +20,6 @@ private extension String {
 
     // MARK: - Type Properties
 
-    static let version = "0.5.11"
+    static let version = "0.5.13"
     static let description = "Generate analytics code for you Swift iOS project"
 }
