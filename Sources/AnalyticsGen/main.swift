723bf5bbf2154366ae9927616fbe958e98b93b6a
v.barabanov <v.barabanov@Brbnkn-MBPro-2.local>
2025-03-06 13:50:46 +0300

    Version updated

--- Sources/AnalyticsGen/main.swift
+++ Sources/AnalyticsGen/main.swift
@@ -20,6 +20,6 @@ private extension String {
 
     // MARK: - Type Properties
 
-    static let version = "0.6.1"
+    static let version = "0.6.2"
     static let description = "Generate analytics code for you Swift iOS project"
 }
