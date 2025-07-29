e584900c744cc662dc3d70862463501d30d7d7a9
Виталий Барабанов <hurmavbatone+hhGit@gmail.com>
2025-07-29 16:57:06 +0300

    Added merge of master into the branch on CI (#80)
    * Added merge of master into the branch on CI
    
    * Autoresolving conflicts removed and env var added
    
    ---------
    
    Co-authored-by: v.barabanov <v.barabanov@Brbnkn-MBPro-4.local>

--- Sources/AnalyticsGen/Providers/Forgejo/ForgejoRemoteRepoProvider.swift
+++ Sources/AnalyticsGen/Providers/Forgejo/ForgejoRemoteRepoProvider.swift
@@ -41,6 +41,13 @@ struct ForgejoRemoteRepoProvider: RemoteRepoProvider {
             case .tag(let name), .branch(let name):
                 try shell("git clone --depth 1 -b \(name) \(gitRepositoryURL) \(repositoryPath)")
 
+                if ProcessInfo.processInfo.environment["ANALYTICS_GEN_EXPERIMENTAL_MERGE"] == "true" &&
+                    ProcessInfo.processInfo.environment["CHANGE_TARGET"] == "develop"
+                {
+                    Log.debug("Trying to merge master into user branch")
+                    try shell("cd \(repositoryPath) && git fetch --depth 1 origin refs/heads/master:refs/remotes/origin/master")
+                    try shell("cd \(repositoryPath) && git merge origin/master --allow-unrelated-histories --no-edit")
+                }
             case .commit(let sha):
                 try shell("git clone \(gitRepositoryURL) \(repositoryPath)")
 
