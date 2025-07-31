bc66a68add1d235629e8ce6085e3e7e9289d62db
Виталий Барабанов <hurmavbatone+hhGit@gmail.com>
2025-07-31 16:28:32 +0300

    Version up (#81)
    Co-authored-by: v.barabanov <v.barabanov@Brbnkn-MBPro-4.local>

--- Sources/AnalyticsGen/main.swift
+++ Sources/AnalyticsGen/main.swift
@@ -20,6 +20,6 @@ private extension String {
 
     // MARK: - Type Properties
 
-    static let version = "0.6.5"
+    static let version = "0.6.6"
     static let description = "Generate analytics code for you Swift iOS project"
 }
